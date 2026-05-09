389c22589c04776c8b669898609cd5e4533e94b5
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2026-05-09 22:32:15 +0530

    u

---
+++ src/main/java/DSA_Pattern_Wise/Promgramming_Basics/Problem3_Good_Turn.java
@@ -0,0 +1,63 @@
+package DSA_Pattern_Wise.Promgramming_Basics;
+
+import java.util.Scanner;
+
+/*
+Problem: Good Turn
+
+Chef and Chefina roll their dice together.
+
+A turn is considered good if the sum of the numbers
+on both dice is greater than 6.
+
+Given X and Y, determine whether the turn is good.
+
+Input:
+- First line contains integer T (number of test cases)
+- Each test case contains two integers X and Y
+
+Output:
+- Print "YES" if X + Y > 6
+- Otherwise print "NO"
+
+Example:
+Input:
+4
+1 4
+3 4
+4 2
+2 6
+
+Output:
+NO
+YES
+NO
+YES
+
+Time Complexity: O(T)
+Space Complexity: O(1)
+*/
+
+public class Problem3_Good_Turn {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            if (X + Y > 6) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+}
